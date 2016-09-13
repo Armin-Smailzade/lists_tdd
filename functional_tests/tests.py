2e683cbe811410ee1af99571a524a1be057a41d9
armin <armin.esmaeilzadeh@gmail.com>
2016-09-12 22:39:34 -0700

    added list template and new url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -18,7 +18,8 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		# self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		self.assertIn('To-Do', self.browser.title)
 		header_test = self.browser.find_element_by_tag_name('h1').text
@@ -28,8 +29,10 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
 
 		inputbox.send_keys("Buy peacock feathers")
-
 		inputbox.send_keys(Keys.ENTER)
+
+		armin_list_url = self.browser.current_url
+		self.assertRegex(armin_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -39,6 +42,26 @@ class NewVisitorTest(unittest.TestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+		## a new user comes in
+		self.browser.quit()
+		self.browser = webdriver.Chrome()
+
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+    
+		john_list_url = self.browser.current_url
+		self.assertRegex(john_list_url, '/lists/.+')
+		self.assertNotEqual(john_list_url, armin_list_url)
+
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		self.fail('Finish the test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -21,41 +21,7 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
-
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(response.content.decode(), expected_html)
-
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = "The first list item"
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = "The Second item"
-		second_item.save()
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first list item')
-		self.assertEqual(second_saved_item.text, 'The Second item')
+	
 
 	def test_home_page_only_saves_items_when_necessary(self):
 		request = HttpRequest()
@@ -81,14 +47,46 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only/')
 
-	def test_home_page_displays_all_list_items(self):
+class ListViewTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_displays_all_items(self):
 		Item.objects.create(text='item1')
 		Item.objects.create(text='item2')
 
-		request = HttpRequest()
-		response = home_page(request)
+		# instead of calling the view function use client 
+		response = self.client.get('/lists/the-only/')
 
-		self.assertIn('item1', response.content.decode())
-		self.assertIn('item2', response.content.decode())
+		# instead of using assert in and decoding stuff
+		self.assertContains(response, 'item1')
+		self.assertContains(response, 'item2')
+
+class ItemModelTest(TestCase):
+
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only/')
+		self.assertTemplateUsed(response, 'list.html')
+
+
+	def test_saving_and_retrieving_items(self):
+
+		first_item = Item()
+		first_item.text = "The first list item"
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = "The Second item"
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first list item')
+		self.assertEqual(second_saved_item.text, 'The Second item')
